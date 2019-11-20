ed2c580735ca07658bf0e8b5a82d08e006a0e99d
tenajesus <tenajesus@hotmail.com>
2019-11-20 08:38:08 -0600

    Mi primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OperacionesComponent } from './operaciones/operaciones.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OperacionesComponent
   ],
   imports: [
     BrowserModule,
